26ffeaba217a1002cd756a050c687166c2cd7909
bhaskar4k <94591414+bhaskar4k@users.noreply.github.com>
2023-05-12 19:16:00 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,26 +1,48 @@
-class DSU{
-private:
-    vector<int> Parent;
-    vector<int> Rank;
+class DisjointSet {
+	vector<int> rank, parent, size; 
 
-public:    
-    DSU(int n) {
-        Parent.resize(n);
-        Rank.resize(n,0);
-        for(int i=0; i<n; i++) Parent[i]=i;
-    }
-
-    int Find(int x) {
-        return Parent[x]=(Parent[x]==x) ? x : Find(Parent[x]);
-    }
-
-    bool Union(int x, int y) {
-        int xset=Find(x), yset=Find(y);
-        if(xset!=yset){
-            (Rank[xset]<Rank[yset]) ? Parent[xset]=yset : Parent[yset]=xset;
-            Rank[xset]+=(Rank[xset]==Rank[yset]);
-            return true;
+public: 
+    DisjointSet(int n){
+        rank.resize(n+1,0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i=0; i<=n; i++){
+            parent[i]=i; 
+            size[i]=1; 
         }
-        return false;
+    }
+
+    int FindParent(int node) {
+        if(node==parent[node]) return node;           
+        return parent[node]=FindParent(parent[node]); 
+    }
+
+    void UnionByRank(int u, int v) {
+        int ulp_u=FindParent(u); 
+        int ulp_v=FindParent(v); 
+        if(ulp_u==ulp_v) return;
+
+        if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u]=ulp_v; 
+        }else if(rank[ulp_v]<rank[ulp_u]){
+            parent[ulp_v]=ulp_u; 
+        }else{
+            parent[ulp_v]=ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void UnionBySize(int u, int v) {
+        int ulp_u=FindParent(u); 
+        int ulp_v=FindParent(v); 
+        if(ulp_u==ulp_v) return; 
+
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v; 
+            size[ulp_v]+=size[ulp_u]; 
+        }else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v]; 
+        }
     }
 };
